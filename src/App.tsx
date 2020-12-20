020af576e9061767a702241175c32675e2ff2c00
Stephane Leclercq <leclercq@ekino.com>
2020-12-20 23:59:22 +0100

    Implement harcoded one-shot ticker data from binance

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from 'axios'
+
+interface TickerData {
+    symbol: string,
+    price: number
+}
+
+interface TickerProps {
+    symbol: string
+}
+
+const Ticker: React.FC<TickerProps> = ({symbol}) => {
+    const [tickerData, setTickerData] = useState<TickerData | undefined>(undefined)
+
+    useEffect(() => {
+        axios.get<TickerData>(`https://api.binance.com/api/v3/ticker/price?symbol=${symbol}`)
+            .then(res => setTickerData(res.data))
+    }, [symbol])
+
+    return <p>I'm a ticker : {tickerData?.price}</p>
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Ticker symbol='BTCUSDT' />
+        </div>
+    );
 }
 
 export default App;
