b7d8cb1e20a26c32c924178d31fb548aeee60d56
Stephane Leclercq <leclercq@ekino.com>
2020-12-23 12:40:21 +0100

    Refactor animations (contrib)

--- src/components/StreamingTicker.tsx
+++ src/components/StreamingTicker.tsx
@@ -1,5 +1,5 @@
-import {BinanceMiniTicker, TickerData, TickerProps} from "../types";
-import React, {useEffect, useState} from "react";
+import { BinanceMiniTicker, TickerData, TickerProps } from "../types";
+import React, { useEffect, useState } from "react";
 import binanceService from "../services/binanceService";
 
 function toTickerData(binanceMiniTicker: BinanceMiniTicker): TickerData {
@@ -12,7 +12,8 @@ function toTickerData(binanceMiniTicker: BinanceMiniTicker): TickerData {
 
 
 const StreamingTicker: React.FC<TickerProps> = ({ symbol }) => {
-    const [tickerClass, setTickerClass] = useState<string>('StreamingTicker-symbol-price')
+    const [tickerClass, setTickerClass] = useState<string>('')
+    const [animationCount, setAnimationCount] = useState<number>(0)
 
     const [previousTickerData, setPreviousTickerData] = useState<TickerData | undefined>(undefined)
     const [tickerData, setTickerData] = useState<TickerData | undefined>(undefined)
@@ -39,23 +40,24 @@ const StreamingTicker: React.FC<TickerProps> = ({ symbol }) => {
             return
         }
         if (Math.trunc(tickerData.price) > Math.trunc(previousTickerData.price)) {
-            setTickerClass(tickerClass === 'StreamingTicker-symbol-price-green' ?
-                'StreamingTicker-symbol-price-green-2' : 'StreamingTicker-symbol-price-green')
+            setTickerClass('up')
+            setAnimationCount(value => (value + 1) % 2)
         }
         if (Math.trunc(tickerData.price) < Math.trunc(previousTickerData.price)) {
-            setTickerClass(tickerClass === 'StreamingTicker-symbol-price-red' ?
-                'StreamingTicker-symbol-price-red-2' : 'StreamingTicker-symbol-price-red')
+            setTickerClass('down')
+            setAnimationCount(value => (value + 1) % 2)
         }
         setPreviousTickerData(tickerData)
     }, [tickerData, previousTickerData, tickerClass])
 
     const truncatedPrice: number = tickerData?.price ? Math.trunc(tickerData.price) : 0
+    const animationName = tickerClass === '' ? '' : `${tickerClass}-${animationCount}`
 
     return (
         <table>
             <tr className='StreamingTicker-symbol'>
                 <td className='StreamingTicker-symbol-name'>{tickerData?.symbol}</td>
-                <td className={tickerClass}>{truncatedPrice}</td>
+                <td className={`StreamingTicker-symbol-price ${animationName}`}>{truncatedPrice}</td>
             </tr>
         </table>
     )
